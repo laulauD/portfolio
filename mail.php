3540ab5489d07fdf65f3d18fc51e51c3b6a315cc
laulauD <laurine.dewaele@epitech.eu>
2021-07-11 22:45:08 +0200

    try mail

--- mail.php
+++ mail.php
@@ -1,22 +1,31 @@
-
 <?php
-ini_set("SMTP", "autoconfig.dewaelelaurine.fr");
-$to      = 'laulaudewaele@gmail.com';
-$subject = 'Nouveau contact';
-$message = '';
-$headers = 'From: contact@portfolio.com';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require "PHPMailer/src/Exception.php";
+require "PHPMailer/src/PHPMailer.php";
+require "PHPMailer/src/SMTP.php";
+
+$mail = new PHPMailer;
+
+
+$mail->From = 'contact@portfolio.com';
+$mail->FromName = 'Contact';
+$mail->addAddress('laulaudewaele@gmail.com');
+
+$mail->isHTML(true);
+
+$mail->Subject = 'Nouveau message !';
 
 if(isset($_POST['submit']) && isset($_POST["message"]) && isset($_POST["usermail"]) && isset($_POST["username"])) {
-    $message = "Mail : ".htmlspecialchars($_POST["usermail"])." --- Nom : ".htmlspecialchars($_POST["username"])." --- message : ".htmlspecialchars($_POST["message"]) ;
-    $result = mail($to, $subject, $message, $headers);
-    if( $result ) {
-        $_POST['erreur'] = false;
-    }else{
-        $_POST['erreur'] = true;
+    $mail->Body = "Mail : ".htmlspecialchars($_POST["usermail"])." --- Nom : ".htmlspecialchars($_POST["username"])." --- message : ".htmlspecialchars($_POST["message"]) ;
+    if (!$mail->send()) {
+        echo 'Le message n\'a pas été envoyé';
+        echo 'Erreur : ' . $mail->ErrorInfo;
+    } else {
+        echo 'Youpi !! Message envoyé !';
     }
 }
-header("Location: index.html");
-exit();
-
+//header("Location: index.html");
+//exit();
 ?>
-
